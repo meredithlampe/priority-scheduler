bf35deaa735db9f59fab8474fad93335ab25329f
Meredith Lampe <merlam@fb.com>
2018-11-27 23:27:56 -0500

    favicon

--- public/main.js
+++ public/main.js
@@ -140,7 +140,21 @@ function addAppointmentsByPriorityText(apptsByPriority) {
 
 function scheduleAppointmentsByPriority(apptsByPriority) {
   addSchedulingText();
-  // do scheduling here!
+  apptsByPriority.forEach(function(applicantList, priority) {
+    let shuffledAppointments = shuffle(applicantList);
+  });
+}
+
+/**
+ * Shuffles array in place. ES6 version
+ * @param {Array} a items An array containing the items.
+ */
+function shuffle(a) {
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
 }
 
 function addSchedulingText() {
